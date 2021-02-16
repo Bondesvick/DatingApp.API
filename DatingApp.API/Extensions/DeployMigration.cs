333407379e2865b062f53452b347512079d52686
Ugoo <54181133+Bondesvick@users.noreply.github.com>
2021-02-16 18:33:34 +0100

    update conn string

---
+++ DatingApp.API/Extensions/DeployMigration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DatingApp.API.Extensions
+{
+    public static class DeployMigration
+    {
+        public static IWebHost MigrateDatabase(this IWebHost webHost)
+        {
+            // Manually run any pending migrations if configured to do so.
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            if (env == "Production")
+            {
+                var serviceScopeFactory = (IServiceScopeFactory)webHost.Services.GetService(typeof(IServiceScopeFactory));
+                using (var scope = serviceScopeFactory.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    var dbContext = services.GetRequiredService<DataContext>();
+                    dbContext.Database.Migrate();
+                }
+            }
+            return webHost;
+        }
+    }
+}
